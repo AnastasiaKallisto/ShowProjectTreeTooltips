8f6b2d3dc47b0d78b73b5c2d6388468b3210486b
Anastasya Kallisto <anastasya.kallisto@gmail.com>
2025-04-12 16:29:43 +0600

    namespace bug fixed

--- src/rider/main/java/com/jetbrains/rider/plugins/showprojecttreetooltips/TooltipUtils.java
+++ src/rider/main/java/com/jetbrains/rider/plugins/showprojecttreetooltips/TooltipUtils.java
@@ -7,6 +7,7 @@ import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiManager;
 import com.jetbrains.rider.languages.fileTypes.csharp.psi.CSharpDocComment;
+import com.jetbrains.rider.languages.fileTypes.csharp.psi.CSharpDummyBlock;
 import com.jetbrains.rider.languages.fileTypes.csharp.psi.CSharpNamespaceDeclaration;
 import com.jetbrains.rider.languages.fileTypes.csharp.psi.impl.CSharpDummyDeclaration;
 import org.jetbrains.annotations.NotNull;
@@ -30,18 +31,46 @@ public class TooltipUtils {
         return null;
     }
 
+    /**
+     * <summary>
+     * Извлекает текст XML-документации (тег <b>&lt;summary&gt;</b>) из первого найденного C# класса,
+     * структуры или интерфейса в указанном файле. <br/>
+     * Сначала пытается найти такие элементы внутри пространства имён, если оно присутствует.
+     * Если пространства имён нет, выполняет поиск в корне файла.
+     * Возвращает текст комментария (без ///, но с &lt;br/&gt;)
+     * </summary>
+     *
+     * @param virtualFile Файл .cs, из которого нужно извлечь документацию.
+     * @param project     Текущий проект, необходимый для получения PSI.
+     * @return Строка с текстом документации или null, если комментарий не найден.
+     */
     public static String extractSummary(VirtualFile virtualFile, @NotNull Project project) {
         PsiFile psiFile = PsiManager.getInstance(project).findFile(virtualFile);
-        Optional<@NotNull PsiElement> first = Arrays.stream(psiFile.getChildren())
-                .flatMap(element -> Arrays.stream(element.getChildren()))
-                .filter(child -> child instanceof CSharpDummyDeclaration)
+
+        Optional<@NotNull PsiElement> csharpElement;
+
+        var maybeNamespace = Arrays.stream(psiFile.getChildren())
+                .filter(child -> child instanceof CSharpNamespaceDeclaration)
                 .findFirst();
-        if (first.isPresent() && first.get() instanceof CSharpNamespaceDeclaration) {
-            // если namespace, добавляем его детей
-            first = Optional.ofNullable(first.get().getFirstChild());
+        if (maybeNamespace.isPresent()){
+            csharpElement = maybeNamespace
+                    .map(namespace -> Arrays.stream(namespace.getChildren())
+                            .filter(child -> child instanceof CSharpDummyBlock)
+                            .findFirst()
+                            .map(block -> Arrays.stream(block.getChildren())
+                                    .filter(child -> child instanceof CSharpDummyDeclaration)
+                                    .findFirst()
+                                    .orElse(null))
+                            .orElse(null)
+                    );
+        } else {
+            csharpElement = Arrays.stream(psiFile.getChildren())
+                    .flatMap(element -> Arrays.stream(element.getChildren()))
+                    .filter(child -> child instanceof CSharpDummyDeclaration)
+                    .findFirst();
         }
-        if (first.isPresent()) {
-            CSharpDocComment docComment = ((CSharpDummyDeclaration) first.get()).getDocComment();
+        if (csharpElement != null && csharpElement.isPresent()) {
+            CSharpDocComment docComment = ((CSharpDummyDeclaration) csharpElement.get()).getDocComment();
             if (docComment != null)
                 return docComment.getMeaningfulText();
         }
